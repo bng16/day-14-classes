64f133ea34d86953f62839ccc51abc729dc2d57e
bng16 <biswajitnag161619@gmail.com>
2024-07-27 13:56:04 +0530

    done

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-class person{
-    constructor(name,age){
-        this.name = name;
+// class person{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greeting(){
+//         return `hello my name is ${this.name} and my age is ${this.age}`;
+//     }
+//     newAge(age){
+//         this.age = age;
+//     }
+//     static randomId(){
+//         return Math.floor(Math.random()*100)
+//     }
+// }
+
+// const bng=new person("Biswajit",22);
+// console.log(bng.greeting());
+// console.log(bng.randomId())
+
+// bng.newAge(23);
+// console.log(bng.greeting());
+
+
+
+// class student extends person{
+//     constructor(name,age,studentId){
+//         super(name,age);
+//         this.studentId = studentId;
+//     }
+
+//     greeting(){
+//         return `hello my name is ${this.name} and my id is ${this.studentId}`;
+//     }
+
+// }
+
+// const student2 = new student('biswajit',22,'20IUT0010031');
+// console.log(student2.greeting());
+
+
+
+class student{
+    constructor(firstName,lastName,age){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
+        student.addCount();
     }
-    greeting(){
-        return `hello my name is ${this.name} and my age is ${this.age}`;
+
+    static addCount(){
+        student.stdCount += 1;
     }
-    newAge(age){
-        this.age = age;
+    static studentCount(){
+        return student.stdCount;
+    }
+    get fullName(){
+        return this.firstName + " " + this.lastName;
     }
 }
-
-const bng=new person("Biswajit",22);
-console.log(bng.greeting());
-
-bng.newAge(23);
-console.log(bng.greeting());
+student.stdCount=0;
+const bng=new student("Biswajit","Nag","22");
+const sng=new student("Sayanjit","Nag","19");
+console.log(student.studentCount());
+console.log(bng.fullName);
